b4b64a4561aa50b98ba6f90fe631fea1e624aa64
dbuhl <dbuhl@ucsc.edu>
2025-09-11 12:06:11 -0700

    updated vr plot

--- volume_rendering/src/vr.py
+++ volume_rendering/src/vr.py
@@ -3,11 +3,6 @@ import numpy as np
 import pyvista as pv
 import dbuhlMod as db
 from netCDF4 import MFDataset
-from pyvista import examples
-
-# pyvista tutorial for a volumetric data plots with opacity
-#grid = examples.load_hydrogen_orbital(3,2,-2)
-#grid.plot(volume=True, opacity=[1,0,1],cmap='magma')
 
 fn = 'simdat2.cdf'
 cdf_file = MFDataset(fn)
@@ -20,6 +15,9 @@ Nx = len(x)
 Ny = len(y)
 Nz = len(z)
 Nt = len(t)
+del x
+del y
+del z
 gx = cdf_file.variables["Gammax"][0]
 gy = cdf_file.variables["Gammay"][0]
 gz = cdf_file.variables["Gammaz"][0]
@@ -27,17 +25,23 @@ dx = gx/Nx
 dy = gy/Ny
 dz = gz/Nz
 
-ux =  np.array(cdf_file.variables["ux"][:])
-uy =  np.array(cdf_file.variables["uy"][:])
-
-vortz = db.FD6X(uy,Nx,dx) - db.FD6Y(ux,Ny,dy)
-vortz = np.transpose(vortz, axes=(0,3,2,1))
-vortz_max = 2*np.abs(ux).max()
-
-opacity = [.6, .3, 0, .3, .6]
-tf = pv.opacity_transfer_function(opacity, 256).astype(float) / 255.0
 ptstp = -1
+ux =  np.array(cdf_file.variables["ux"][:])
+ux_inst =  ux[ptstp,:,:,:]
+del ux
+uy =  np.array(cdf_file.variables["uy"][:])
+uy_inst =  uy[ptstp,:,:,:]
+del uy
+
+
+vortz_max = 2*np.abs(ux_inst).max()
+vortz = db.iFD6X(uy_inst,Nx,dx) - db.iFD6Y(ux_inst,Ny,dy)
+vortz = np.transpose(vortz, axes=(2,1,0))
+
+opacity = [.3, 0, 0, 0, .3]
+tf = pv.opacity_transfer_function(opacity, 256).astype(float) / 255.0
 pl = pv.Plotter(border=False)
-pl.add_volume(vortz[ptstp,:,:,:],clim=[-vortz_max,vortz_max],cmap='RdYlBu_r',opacity=opacity)
+pl.add_volume(vortz,clim=[-vortz_max,vortz_max],cmap='RdYlBu_r',opacity=opacity)
+pl.add_bounding_box()
 pl.show()
 
