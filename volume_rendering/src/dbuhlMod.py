0cabd7a5907e7102df5e5cb61ad21cd6e06eaf47
Dante Buhl <dbuhl@ucsc.edu>
2025-09-26 09:46:28 -0700

    updated volume rendering directory with latest code

--- volume_rendering/src/dbuhlMod.py
+++ volume_rendering/src/dbuhlMod.py
@@ -108,19 +108,42 @@ def FD6Y(field,ny,dy):
                                            +field[:,:,i,:])
     return -dy_field
 
-def iFD4Z(field,nz,dz):
-    # takes in a field dataset with 4 dimensions (3 spatial, 1 time)
+def iFD6Z(field,nz,dz):
+    # takes in a field dataset with 4 dimensions (3 spatial)
     # returns dataset (numpy) of same shape but computing the 1st derivative
     # with respect to x_comp
     dz_field = np.zeros_like(field)
-    dz_field[0,:,:] = (field[1,:,:]-field[0,:,:])/dz
-    dz_field[1,:,:] = (field[2,:,:]-field[0,:,:])/(2*dz)
-    dz_field[nz-2,:,:] = (field[nz-1,:,:]-field[nz-3,:,:])/(2*dz)
-    dz_field[nz-1,:,:] = (field[nz-1,:,:]-field[nz-2,:,:])/dz
+    dz_field[nz-1,:,:] = (1/(60*dz))*(-field[2,:,:]+9*field[1,:,:]\
+                           -45*field[0,:,:]\
+                           +45*field[nz-2,:,:]-9*field[nz-3,:,:]\
+                           +field[nz-4,:,:])
+    dz_field[nz-2,:,:] = (1/(60*dz))*(-field[1,:,:]+9*field[0,:,:]\
+                           -45*field[nz-1,:,:]\
+                           +45*field[nz-3,:,:]-9*field[nz-4,:,:]\
+                           +field[nz-5,:,:])
+    dz_field[nz-3,:,:] = (1/(60*dz))*(-field[0,:,:]+9*field[nz-1,:,:]\
+                           -45*field[nz-2,:,:]\
+                           +45*field[nz-4,:,:]-9*field[nz-5,:,:]\
+                           +field[nz-6,:,:])
+
+    dz_field[2,:,:] = (1/(60*dz))*(-field[5,:,:]+9*field[4,:,:]\
+                           -45*field[3,:,:]\
+                           +45*field[1,:,:]-9*field[0,:,:]\
+                           +field[nz-1,:,:])
+    dz_field[1,:,:] = (1/(60*dz))*(-field[4,:,:]+9*field[3,:,:]\
+                           -45*field[2,:,:]\
+                           +45*field[0,:,:]-9*field[nz-1,:,:]\
+                           +field[nz-2,:,:])
+    dz_field[0,:,:] = (1/(60*dz))*(-field[3,:,:]+9*field[2,:,:]\
+                           -45*field[1,:,:]\
+                           +45*field[nz-1,:,:]-9*field[nz-2,:,:]\
+                           +field[nz-3,:,:])
     # 4th order centered finite difference formula (inside)
-    for i in range(nz-4):
-        dz_field[i+2,:,:] = (1/(12*dz))*(-field[i+4,:,:]+8*field[i+3,:,:]\
-                                           -8*field[i+1,:,:]+field[i,:,:])
+    for i in range(nz-6):
+        dz_field[i+3,:,:] = (1/(60*dz))*(-field[i+6,:,:]+9*field[i+5,:,:]\
+                                           -45*field[i+4,:,:]\
+                                           +45*field[i+2,:,:]-9*field[i+1,:,:]\
+                                           +field[i,:,:])
     return dz_field
 
 def iFD6X(field,nx,dx):
@@ -128,15 +151,6 @@ def iFD6X(field,nx,dx):
     # returns dataset (numpy) of same shape but computing the 1st derivative
     # with respect to x_comp
     dx_field = np.zeros_like(field)
-    # foward / backwards finite difference formula (boundary)
-    #dx_field[:,:,:,0] = (field[:,:,:,1]-field[:,:,:,0])/dx
-    #dx_field[:,:,:,1] = (field[:,:,:,2]-field[:,:,:,0])/(2*dx)
-    #dx_field[:,:,:,2] = (-field[:,:,:,4]+8*field[:,:,:,3] \
-                        #-8*field[:,:,:,1]+field[:,:,:,0])/(12*dx)
-    #dx_field[:,:,:,nx-3] = (-field[:,:,:,nx-1]+8*field[:,:,:,nx-2]\
-                            #-8*field[:,:,:,nx-5]+field[:,:,:,nx-6])/(12*dx)
-    #dx_field[:,:,:,nx-2] = (field[:,:,:,nx-1]-field[:,:,:,nx-3])/(2*dx)
-    #dx_field[:,:,:,nx-1] = (field[:,:,:,nx-1]-field[:,:,:,nx-2])/dx
     dx_field[:,:,nx-1] = (1/(60*dx))*(-field[:,:,2]+9*field[:,:,1]\
                            -45*field[:,:,0]\
                            +45*field[:,:,nx-2]-9*field[:,:,nx-3]\
--- volume_rendering/src/vr.py
+++ volume_rendering/src/vr.py
@@ -15,6 +15,7 @@ Nx = len(x)
 Ny = len(y)
 Nz = len(z)
 Nt = len(t)
+print('Number of Timesteps: ', Nt)
 del x
 del y
 del z
@@ -24,6 +25,11 @@ gz = cdf_file.variables["Gammaz"][0]
 dx = gx/Nx
 dy = gy/Ny
 dz = gz/Nz
+Re = 1./cdf_file.variables["D_visc"][0]
+Pe = 1./cdf_file.variables["D_therm"][0]
+B = cdf_file.variables["B_therm"][0]
+Fr = 1./np.sqrt(B)
+print("Parameters: Re - ", Re, ", Pe - ", Pe, ', B - ', B)
 
 ptstp = -1
 ux =  np.array(cdf_file.variables["ux"][:])
@@ -32,16 +38,96 @@ del ux
 uy =  np.array(cdf_file.variables["uy"][:])
 uy_inst =  uy[ptstp,:,:,:]
 del uy
+uz =  np.array(cdf_file.variables["uz"][:])
+uz_inst =  uz[ptstp,:,:,:]
+del uz
+temp =  np.array(cdf_file.variables["Temp"][:])
+temp_inst =  temp[ptstp,:,:,:]
+del temp
 
+cdf_file.close()
 
-vortz_max = 2*np.abs(ux_inst).max()
+dTdz = db.iFD6Z(temp_inst,Nz,dz)
+tdisp = db.iFD6Z(temp_inst,Nz,dz)**2 + db.iFD6Y(temp_inst,Ny,dy)**2 +\
+    db.iFD6X(temp_inst,Nx,dx)**2
+mdisp = db.iFD6Z(ux_inst,Nz,dz)**2 + db.iFD6Y(ux_inst,Ny,dy)**2 +\
+    db.iFD6X(ux_inst,Nx,dx)**2 + db.iFD6Z(uy_inst,Nz,dz)**2 + db.iFD6Y(uy_inst,Ny,dy)**2 +\
+    db.iFD6X(uy_inst,Nx,dx)**2 +db.iFD6Z(uz_inst,Nz,dz)**2 + db.iFD6Y(uz_inst,Ny,dy)**2 +\
+    db.iFD6X(uz_inst,Nx,dx)**2
+Ri = (1 + dTdz)/((Fr**2)*np.minimum((db.iFD6Z(ux_inst,Nz,dz)**2 + db.iFD6Z(uy_inst,Nz,dz)**2),1e-4)) 
+eta = (B*tdisp/Pe)/(B*tdisp/Pe + mdisp/Re)
 vortz = db.iFD6X(uy_inst,Nx,dx) - db.iFD6Y(ux_inst,Ny,dy)
-vortz = np.transpose(vortz, axes=(2,1,0))
 
-opacity = [.3, 0, 0, 0, .3]
-tf = pv.opacity_transfer_function(opacity, 256).astype(float) / 255.0
+idx_lam = np.where(vortz**2 <= 1./Fr)
+idx_turb = np.where(vortz**2 > 1./Fr)
+
+# computing max
+temp_max = np.abs(temp_inst).max()
+dtemp_max = np.abs(dTdz).max()
+tdisp_max = tdisp.max()
+mdisp_max = mdisp.max()
+vortz_max = 2*np.abs(ux_inst).max()
+
+#print('Avg TDisp: ',tdisp.mean())
+#print('Avg MDisp: ',mdisp.mean())
+#print('Avg Eta (local): ',eta.mean())
+#print('Avg Eta (global): ', (B*tdisp.mean()/Pe)/\
+    #(B*tdisp.mean()/Pe + mdisp.mean()/Re))
+#print('Avg Lam Eta (local): ', eta[idx_lam].mean(), ' +/- ', eta[idx_lam].std())
+#print('Avg Lam Eta (global): ', (B*tdisp[idx_lam].mean()/Pe)/\
+    #(B*tdisp[idx_lam].mean()/Pe + mdisp[idx_lam].mean()/Re))
+#print('Avg Turb Eta (local): ', eta[idx_turb].mean(), ' +/- ', eta[idx_turb].std())
+#print('Avg Turb Eta (global): ', (B*tdisp[idx_turb].mean()/Pe)/\
+    #(B*tdisp[idx_turb].mean()/Pe + mdisp[idx_turb].mean()/Re))
+#print('VLam: ', len(idx_lam[0])/(Nx*Ny*Nz))
+#print('VTurb: ', len(idx_turb[0])/(Nx*Ny*Nz))
+
+# transposing 
+vortz = np.transpose(vortz, axes=(2,1,0))
+dTdz = np.transpose(dTdz, axes=(2,1,0))
+Ri = np.transpose(Ri, axes=(2,1,0))
+ux_inst = np.transpose(ux_inst,axes=(2,1,0))
+temp_inst = np.transpose(temp_inst,axes=(2,1,0))
+eta = np.transpose(eta,axes=(2,1,0))
+mdisp = np.transpose(mdisp,axes=(2,1,0))
+tdisp = np.transpose(tdisp,axes=(2,1,0))
+
 pl = pv.Plotter(border=False)
-pl.add_volume(vortz,clim=[-vortz_max,vortz_max],cmap='RdYlBu_r',opacity=opacity)
+
+#ux plot
+#opacity = [.3,0,0,0,.3]
+#pl.add_volume(ux_inst,clim=[-vortz_max/2,vortz_max/2],cmap='RdYlBu_r',opacity=opacity)
+#print('UX max :', vortz_max/2)
+
+#vort plot
+#opacity = [.3,0,0,0,.3]
+#pl.add_volume(vortz,clim=[-vortz_max,vortz_max],cmap='RdYlBu_r',opacity=opacity)
+#print('Vortz max :', vortz_max)
+
+# temp plot
+#opacity = [.3,0,0.05,0,.3]
+#pl.add_volume(temp_inst,clim=[-temp_max,temp_max],cmap='RdYlBu_r',opacity=opacity)
+
+#Ri Plot
+#opacity = [.1,.1,.3,0,0]
+#pl.add_volume(Ri,clim=[-0.75,1.25],cmap='RdYlBu_r',opacity=opacity)
+
+# mdisp Plot
+#opacity = [0,0,.1,.3,.3]
+#pl.add_volume(mdisp/Re,clim=[0,mdisp_max/(2*Re)],cmap='viridis_r',opacity=opacity)
+#print('mdisp max: ', mdisp_max/(2*Re))
+
+# tdisp Plot
+opacity = [0,0,.1,.3,.3]
+pl.add_volume(tdisp,clim=[0,tdisp_max/2],cmap='viridis_r',opacity=opacity)
+#print('tdisp max: ', tdisp_max/2)
+
+# eta Plot
+#opacity = [.01,.01,.01,0,.4]
+#pl.add_volume(eta,clim=[0,1],cmap='RdYlBu_r',opacity=opacity)
+
 pl.add_bounding_box()
+#pl.view_xz()
+pl.set_viewup([0,1,0])
 pl.show()
 
