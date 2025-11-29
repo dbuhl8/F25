8eb4781bc12063882f2bf2376c4e824904786b66
dbuhl <dbuhl@ucsc.edu>
2025-11-28 17:59:55 -0800

    moving to arch desktop

--- AM216/hw/hw8/hw8_p5_code.py
+++ AM216/hw/hw8/hw8_p5_code.py
@@ -2,25 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-
-# problem 6 diagnostics
-
-sigma = 1
+X_0 = 0.7
 dt = 2**(-10)
 nt = int(1./dt)
-
-
-X_0 = 0.7
-
-t = np.zeros(nt)
-X_sde = np.zeros(nt)
-weight = np.zeros(nt)
-
 trials = 10**5
 
-fig, ax = plt.subplots()
-
 mu = 0 
+t = np.zeros(nt+1)
+X_sde = np.zeros(nt+1)
+weight = np.zeros(nt+1)
+
+fig, ax = plt.subplots()
 
 def phi(x,t):
     return 0.5*((x-t)**2 + 2*(x-t) - 1)
@@ -28,18 +20,23 @@ def BC(x,t):
     return np.exp(-(x - t)**2/2)
 
 for j in range(trials):
-    dW = np.random.normal(0,dt,size=nt-1)
+    dW = np.random.normal(0,np.sqrt(dt),size=nt)
 
     t[0] = 0 
     X_sde[0] = X_0
     weight[0] = 1
-    phi
 
-    for i in range(nt-1):
+    for i in range(nt):
+        counter = i
         t[i+1] = t[i] + dt
         X_sde[i+1] = X_sde[i] + dW[i]
-        weight[i+1] = weight[i]*np.exp(phi(X_sde[i],t[i]))*dt
-    mu += weight[-1]*BC(X_sde[-1],1)
+        weight[i+1] = weight[i]*np.exp(-phi(X_sde[i],t[i])*dt)
+        if (weight[i+1] == 0):
+            print('Loop Index: (i, j) = (',i,',',j,')')
+            print('Diagnostic: ',t[i+1],X_sde[i+1],weight[i+1])
+            break
+    mu_update = weight[-1]*BC(X_sde[-1],1)
+    mu += mu_update
         
 mu /= trials
 print('Average Value: ', mu)
